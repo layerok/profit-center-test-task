b6c519e2dc938feb2d811951ad7fa46f7524daa3
Vladimir <kotopes231@gmail.com>
2023-07-14 20:29:02 +0300

    refactor

--- src/routes/stat/stat.route.tsx
+++ src/routes/stat/stat.route.tsx
@@ -10,94 +10,92 @@ import { getStat } from "../../api/stats.api";
 export const StatRoute = () => {
   const params = useParams();
   const navigate = useNavigate();
-  const back = () => {
+
+  const goBackToList = () => {
     navigate(routePaths.stats);
   };
 
   const { data: stat, isLoading } = useQuery<DbStat>({
     queryKey: ["stat", params.id],
     queryFn: () => {
-      return getStat(+(params.id as string));
+      return getStat(Number(params.id as string));
     },
   });
+
+  if (isLoading) {
+    return <div>"...loading"</div>;
+  }
+  if (!stat) {
+    return <div>No data</div>;
+  }
   return (
     <>
-      {isLoading ? (
-        "...loading"
-      ) : stat ? (
-        <>
-          <S.Header>
-            <ArrowLeft
-              style={{
-                cursor: "pointer",
-              }}
-              onClick={back}
-            />
-            <S.Title>Статистика #{params.id}</S.Title>
-          </S.Header>
-          <S.Row
-            style={{
-              marginTop: 38,
-            }}
-          >
-            <S.Prop>
-              <S.Label>Начало вычислений</S.Label>
-              <S.Value>{format(new Date(+stat.start), "hh:mm:ss")}</S.Value>
-            </S.Prop>
-            <S.Prop>
-              <S.Label>Конец вычислений</S.Label>
-              <S.Value>{format(new Date(+stat.end), "hh:mm:ss")}</S.Value>
-            </S.Prop>
-            <S.Prop>
-              <S.Label>Время потраченно</S.Label>
-              <S.Value>
-                {stat.time_spent < 1 ? "<1ms" : stat.time_spent}
-              </S.Value>
-            </S.Prop>
-            <S.Prop>
-              <S.Label>Кол-во котировок</S.Label>
-              <S.Value>{stat.quotes_amount}</S.Value>
-            </S.Prop>
-          </S.Row>
+      <S.Header>
+        <ArrowLeft
+          style={{
+            cursor: "pointer",
+          }}
+          onClick={goBackToList}
+        />
+        <S.Title>Статистика #{params.id}</S.Title>
+      </S.Header>
+      <S.Row
+        style={{
+          marginTop: 38,
+        }}
+      >
+        <S.Prop>
+          <S.Label>Начало вычислений</S.Label>
+          <S.Value>{format(new Date(Number(stat.start)), "hh:mm:ss")}</S.Value>
+        </S.Prop>
+        <S.Prop>
+          <S.Label>Конец вычислений</S.Label>
+          <S.Value>{format(new Date(Number(stat.end)), "hh:mm:ss")}</S.Value>
+        </S.Prop>
+        <S.Prop>
+          <S.Label>Время потраченно</S.Label>
+          <S.Value>{stat.time_spent < 1 ? "<1ms" : stat.time_spent}</S.Value>
+        </S.Prop>
+        <S.Prop>
+          <S.Label>Кол-во котировок</S.Label>
+          <S.Value>{stat.quotes_amount}</S.Value>
+        </S.Prop>
+      </S.Row>
 
-          <S.Row
-            style={{
-              marginTop: 48,
-            }}
-          >
-            <S.Prop>
-              <S.Label>Середне арефметичке</S.Label>
-              <S.Value>{stat.avg}</S.Value>
-            </S.Prop>
-            <S.Prop>
-              <S.Label>Мінімальне значення</S.Label>
-              <S.Value>{stat.min}</S.Value>
-            </S.Prop>
-            <S.Prop>
-              <S.Label>Максимальне значення</S.Label>
-              <S.Value>{stat.max}</S.Value>
-            </S.Prop>
-          </S.Row>
-          <S.Row
-            style={{
-              marginTop: 27,
-            }}
-          >
-            <S.Prop>
-              <S.Label>Стандартное отклонение</S.Label>
-              <S.Value>{stat.standard_deviation}</S.Value>
-            </S.Prop>
-            <S.Prop>
-              <S.Label>Мода</S.Label>
-              <S.Value>
-                {stat.moda} ({stat.moda_count}x)
-              </S.Value>
-            </S.Prop>
-          </S.Row>
-        </>
-      ) : (
-        "no data"
-      )}
+      <S.Row
+        style={{
+          marginTop: 48,
+        }}
+      >
+        <S.Prop>
+          <S.Label>Середне арефметичке</S.Label>
+          <S.Value>{stat.avg}</S.Value>
+        </S.Prop>
+        <S.Prop>
+          <S.Label>Мінімальне значення</S.Label>
+          <S.Value>{stat.min}</S.Value>
+        </S.Prop>
+        <S.Prop>
+          <S.Label>Максимальне значення</S.Label>
+          <S.Value>{stat.max}</S.Value>
+        </S.Prop>
+      </S.Row>
+      <S.Row
+        style={{
+          marginTop: 27,
+        }}
+      >
+        <S.Prop>
+          <S.Label>Стандартное отклонение</S.Label>
+          <S.Value>{stat.standard_deviation}</S.Value>
+        </S.Prop>
+        <S.Prop>
+          <S.Label>Мода</S.Label>
+          <S.Value>
+            {stat.moda} ({stat.moda_count}x)
+          </S.Value>
+        </S.Prop>
+      </S.Row>
     </>
   );
 };
