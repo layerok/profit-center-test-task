0e8c8ff71666a8ad19c3912adb2b76f592d6f4b0
Vladimir <kotopes231@gmail.com>
2023-07-14 20:45:24 +0300

    refactor

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -1,7 +1,3 @@
-import { StatsRoute } from "./routes/stats/stats.route";
-import { HomeRoute } from "./routes/home/home.route";
-import { StatRoute } from "./routes/stat/stat.route";
-import { LayoutRoute } from "./routes/layout/layout";
 import { routePaths } from "./constants/route.constant";
 
 export const appRoutes = [
